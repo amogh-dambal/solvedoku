48bfcce9616658aeabe2db1e7773a881de262701
amogh-dambal <amdam2013@gmail.com>
2021-06-08 17:32:22 -0500

    remove old test backend code

--- backend/src/main/java/com/amoghdambal/solvedoku/repository/FlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.amoghdambal.solvedoku.repository;
-
-import com.amoghdambal.solvedoku.data.Flight;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FlightRepository extends MongoRepository<Flight, Long> {
-}
